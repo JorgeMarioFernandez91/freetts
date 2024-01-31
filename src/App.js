4f789ddfc43e833c50112bf40fb8be10bbd71147
mario <jmferna91@gmail.com>
2024-01-30 20:33:05 -0500

    added initial loading page

--- src/App.js
+++ src/App.js
@@ -4,17 +4,37 @@ import Form from './components/Form.js';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
 import Tutorial from './components/Tutorial.js';
+import { useState } from 'react';
+import Loading from './components/Loading.js';
 
 function App() {
 
-  return (
-    <div className="app">
-      <Header />
-      <Form />
-      <Tutorial />
-      <Footer />
-    </div>
-  );
+  const [loading, setLoading] = useState(true);
+
+  // simulate a 4 second loading time
+  setTimeout(function() {
+    setLoading(false);  
+  }, 4000);
+
+  if (loading) {
+    return (
+      <div className="app">
+        <Loading handleLoading={loading}/>
+      </div>
+    );
+  }
+  else {
+    return (
+      <div className="app">
+        <Header />
+        <Form />
+        <Tutorial />
+        <Footer />
+      </div>
+    );
+  }
+
+  
 }
 
 export default App;
