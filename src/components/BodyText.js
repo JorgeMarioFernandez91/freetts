601730a2e007b83c770a4ffc373242797ecc74db
Jorge Mario Fernandez <36650521+JorgeMarioFernandez91@users.noreply.github.com>
2024-01-31 18:30:51 -0500

    add user types selection page (#3)

---
+++ src/components/BodyText.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../styles/body-text.scss"
+
+function BodyText() {
+    return (
+        <div className="body-text">
+            <p>TTS Engine is a free text-to-speech tool that provides both scripts and the voices to bring those scripts to life! Multiple languages are supported simply select your preferred language and TTS Engine will do the rest.</p>
+        </div>
+    )
+}
+
+export default BodyText
