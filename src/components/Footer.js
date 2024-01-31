1fa78a1f5448863057dbdb29821f6a6c4ce95e27
Jorge Mario Fernandez <36650521+JorgeMarioFernandez91@users.noreply.github.com>
2024-01-31 17:46:38 -0500

    base layout setup (#2)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import "../styles/footer.scss"
 
 function Footer() {
     return (
-        <div>
-            <div>Footer</div>
+        <div className="footer">
+            <a className="github" href="https://www.google.ca" target="_blank" rel="noreferrer">GitHub</a>
+            <a className="website" href="https://www.google.ca" target="_blank" rel="noreferrer">jorgemariofernandez.com</a>
+            <a className="linkedin" href="https://www.google.ca" target="_blank" rel="noreferrer"> linkedin</a>
         </div>
     )
 }
