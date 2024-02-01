45e3a785bd5753f3f1f78d8d7976174340c579f0
mario <jmferna91@gmail.com>
2024-02-01 13:24:09 -0500

    fix background clipping

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,30 +1,38 @@
-import React from 'react'
-import ScriptReader from './ScriptReader'
-import '../styles/body.scss'
-import BodyText from './BodyText'
-import ScriptGenerator from './ScriptGenerator'
-import DecideScriptNeeds from './DecideScriptNeeds'
+import React from "react";
+import ScriptReader from "./ScriptReader";
+import "../styles/body.scss";
+import BodyText from "./BodyText";
+import ScriptGenerator from "./ScriptGenerator";
+import DecideScriptNeeds from "./DecideScriptNeeds";
 
-function Body() {
-    const [userType, SetUserType] = React.useState(null)
-    var component = null;
-
-    if (userType === null) {
-        component = <DecideScriptNeeds handleUserType={SetUserType}/>;
-    }
-    else if (userType === "has-script"){
-        component = <ScriptReader />;
-    }
-    else {
-        component = <ScriptGenerator />;
-    }
-
-    return (
-        <div className="body">
-            <BodyText />
-            {component}
-        </div>
-    )
+function Background() {
+  return (
+    <div className="background">
+      <div className="top-glyph" />
+      <div className="bottom-glyph" />
+    </div>
+  );
 }
 
-export default Body
+function Body() {
+  const [userType, SetUserType] = React.useState(null);
+  var component = null;
+
+  if (userType === null) {
+    component = <DecideScriptNeeds handleUserType={SetUserType} />;
+  } else if (userType === "has-script") {
+    component = <ScriptReader />;
+  } else {
+    component = <ScriptGenerator />;
+  }
+
+  return (
+    <div className="body">
+      <Background />
+      <BodyText />
+      {component}
+    </div>
+  );
+}
+
+export default Body;
